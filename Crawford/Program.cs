88816c48552c0ae7ce5d7c9e99463f6321a57816
Ehtesham <ehteshamkhan@live.ca>
2022-03-16 20:15:07 -0400

    Final version including login

--- Crawford/Program.cs
+++ Crawford/Program.cs
@@ -7,40 +7,71 @@ using System.Threading.Tasks;
 
 namespace Crawford
 {
-   
+
     internal class Program
     {
         static string connectionString = "Server=interview-testing-server.database.windows.net; Database=Interview; User Id=TestLogin; Password=5D9ej2G64s3sd^;";
-    static SqlConnection sqlconn = new SqlConnection(connectionString);
+        static SqlConnection sqlconn = new SqlConnection(connectionString);
+        static SqlConnection sqlconn2 = new SqlConnection(connectionString);
         static void Main(string[] args)
         {
-           
-            StringBuilder sb = new StringBuilder(); 
+
+            Console.WriteLine("Enter UserName: ");
+            string userId = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
             using (sqlconn)
             {
                 try
                 {
                     sqlconn.Open();
+                    sqlconn2.Open();
                     SqlCommand cmd = new SqlCommand("Select LossTypeId, LossTypeCode, LossTypeDescription from LossTypes", sqlconn);
-                   var result = (int)cmd.ExecuteScalar();
-                    var result2 = cmd.ExecuteNonQuery();
+                    SqlCommand cmdLogin = new SqlCommand("Select UserName, Password from Users", sqlconn2);
+                    //var result = (int)cmd.ExecuteScalar();
+                    //var result2 = cmd.ExecuteNonQuery();
                     SqlDataReader reader = cmd.ExecuteReader();
-                    sb.Append(result2);
-                    
-                    if (result >0)
+
+                    //sb.Append(result2);
+
+                    //if (result >0)
+                    //{
+
+                    //    Console.WriteLine("Successful Login");
+                    //    Console.WriteLine(result+" total results");
+                    //}
+
+                    using (SqlDataReader readerLogin = cmdLogin.ExecuteReader())
                     {
-                      
-                        Console.WriteLine("Successful Login");
-                        Console.WriteLine(result+" total results");
-                    }
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
+                        if (readerLogin.HasRows)
                         {
-                            Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetName(0), reader.GetName(1), reader.GetName(2) );
-                            Console.WriteLine("\t{0}\t\t{1}\t\t{2}", reader.GetInt32(0) , reader.GetString(1) , reader.GetString(2));
+                            while (readerLogin.Read())
+                            {
+                                if (readerLogin.GetString(0) == userId && readerLogin.GetString(1) == password)
+                                {
+                                    //readerLogin.Close();
+                                    if (reader.HasRows)
+                                    {
+                                        while (reader.Read())
+                                        {
+                                            Console.WriteLine("\t{0}\t{1}\t{2}", reader.GetName(0), reader.GetName(1), reader.GetName(2));
+                                            Console.WriteLine("\t{0}\t\t{1}\t\t{2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
 
 
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Didnt get any values from database...");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Incorrect Username or Password");
+                                }
+                            }
                         }
                     }
                 }
@@ -54,6 +85,7 @@ namespace Crawford
                     Console.ReadKey();
                 }
                 sqlconn.Close();
+                sqlconn2.Close();
             }
         }
     }
